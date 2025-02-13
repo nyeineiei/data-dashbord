ed39e7daaaae8419b9e3ca87ca7d866a291bf301
Nyein <pauksohne2t@gmail.com>
2025-02-13 16:25:10 +0800

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const App = () => {
+  return <h1>Hello, World!</h1>;
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
